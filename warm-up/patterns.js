c5c3fad55bb7d502887c28ac53021d33e3d4c537
karthick <karthylock@gmail.com>
2023-11-22 15:07:30 +0530

    pattern 9 is completed

--- warm-up/patterns.js
+++ warm-up/patterns.js
@@ -2,60 +2,66 @@ function pattern1(n) {
   let str = "";
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      console.log((str += "*"));
+      str += "*";
     }
-    console.log((str += " \n"));
+    str += " \n";
   }
+  console.log(str);
 }
 
 function pattern2(n) {
   let str = "";
   for (let i = 0; i < n; i++) {
     for (let j = 0; j <= i; j++) {
-      console.log((str += "*"));
+      str += "*";
     }
-    console.log((str += "\n"));
+    str += "\n";
   }
+  console.log(str);
 }
 
 function pattern3(n) {
   let str = "";
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= i; j++) {
-      console.log((str += j));
+      str += j;
     }
-    console.log((str += "\n"));
+    str += "\n";
   }
+  console.log(str);
 }
 
 function pattern4(n) {
   let str = "";
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= i; j++) {
-      console.log((str += i));
+      str += i;
     }
-    console.log((str += "\n"));
+    str += "\n";
   }
+  console.log(str);
 }
 
 function pattern5(n) {
   let str = "";
   for (let i = 0; i < n; i++) {
     for (let j = 0; j <= n - i - 1; j++) {
-      console.log((str += "*"));
+      str += "*";
     }
-    console.log((str += "\n"));
+    str += "\n";
   }
+  console.log(str);
 }
 
 function pattern6(n) {
   let str = "";
   for (let i = 0; i <= n; i++) {
     for (let j = 1; j <= n - i; j++) {
-      console.log((str += j));
+      str += j;
     }
-    console.log((str += "\n"));
+    str += "\n";
   }
+  console.log(str);
 }
 
 function pattern7(n) {
@@ -63,29 +69,59 @@ function pattern7(n) {
   for (let i = 0; i < n; i++) {
     //spaces
     for (let j = 1; j <= n - i; j++) {
-      console.log((str += " "));
+      str += " ";
     }
     //star  1,3,5,7,9 n-5, i - 01234
     for (let j = 1; j <= 2 * i + 1; j++) {
-      console.log((str += "*"));
+      str += "*";
     }
-    console.log((str += "\n"));
+    str += "\n";
   }
+  console.log(str);
 }
 
-function pattern8(n){
-    let str = ''
-    for(let i=0;i<n;i++){
-        //spaces
-        for(let j=1;j<=i;j++){
-            console.log(str+=' ')
-        }
-        //star
-        for(let j=0;j<2*n-2*i-1;j++){
-            console.log(str+='*')
-        }
-        console.log(str+='\n')
+function pattern8(n) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    //spaces
+    for (let j = 1; j <= i; j++) {
+      str += " ";
     }
+    //star
+    for (let j = 0; j < 2 * n - 2 * i - 1; j++) {
+      str += "*";
+    }
+    str += "\n";
+  }
+  console.log(str);
 }
 
-pattern8(5);
+function pattern9(n) {
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    //spaces
+    for (let j = 1; j <= n - i - 1; j++) {
+      str += " ";
+    }
+    //star  1,3,5,7,9 n-5, i - 01234
+    for (let j = 1; j <= 2 * i + 1; j++) {
+      str += "*";
+    }
+    str += "\n";
+  }
+
+  for (let i = 0; i < n; i++) {
+    //spaces
+    for (let j = 1; j <= i; j++) {
+      str += " ";
+    }
+    //star
+    for (let j = 1; j <= 2 * n - 2 * i - 1; j++) {
+      str += "*";
+    }
+    str += "\n";
+  }
+  return str;
+}
+
+console.log(pattern9(5))
