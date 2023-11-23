f073b07b7ee43f6869690763cb161bfdd32bb7ca
karthick <karthylock@gmail.com>
2023-11-23 20:04:29 +0530

    finished divisor in basic maths section

--- basic-math/math.js
+++ basic-math/math.js
@@ -49,4 +49,12 @@ function amstrong(n){
     }
  
 }
-console.log(amstrong(170))
+
+function div(n){
+    for(let i =1;i<=n;i++){
+        if(n%i==0){
+            console.log(i)
+        }
+    }
+}
+console.log(div(36))
