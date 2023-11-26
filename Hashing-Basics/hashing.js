0705bbaf564deb2c3052728afad274c0c81a47f5
karthick <karthylock@gmail.com>
2023-11-26 14:39:56 +0530

    implementing hashing

--- Hashing-Basics/hashing.js
+++ Hashing-Basics/hashing.js
@@ -1,16 +1,36 @@
 // count elements in an array using brute force method
 
-function hashing(arr,num){
+function hashing(arr, num) {
   let count = 0;
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]==num) count++
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == num) count++;
   }
-  return count
+  return count;
 }
 
-function main(){
-    let arr = [1,2,1,3,2]
-    
-    console.log(hashing(arr,10))
+function main() {
+  let arr = [1, 2, 1, 3, 2];
+
+  console.log(hashing(arr, 10));
 }
-main()
+main();
+
+//  using hashing
+
+function hash(arr) {
+  let hash = {};
+
+  for (const element of arr) {
+    if (hash[element]) hash[element]++;
+    else hash[element] = 1;
+  }
+
+  console.log(hash);
+}
+
+function start() {
+  let arr = [1, 2, 2, 1, 4];
+  hash(arr);
+}
+
+start();
