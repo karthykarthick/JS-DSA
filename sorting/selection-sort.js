4ea5f689b5a1d832982c46b608e92401b8fe62f3
karthick <karthylock@gmail.com>
2023-11-28 13:43:58 +0530

    finished selection sort

---
+++ sorting/selection-sort.js
@@ -0,0 +1,24 @@
+function selection(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr
+}
+
+function main() {
+  let arr = [64, 25, 12, 22, 11];
+  console.log(selection(arr))
+}
+
+main()
